9815926b0434c3efe906200e6f621dd7797ca4c8
vinson <1535372340@qq.com>
2021-06-02 22:40:17 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  </head>
-  <body>
-    @include('layouts._header')
+    <head>
+        <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        @include('layouts._header')
 
-    <div class="container">
-      @yield('content')
-      @include('layouts._footer')
-    </div>
-  </body>
+        <div class="container">
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </body>
 </html>
